02cd670dc891a0c2da3ae398363126c09904b838
dangowans <d.gowans@cityssm.on.ca>
2022-03-04 14:23:09 -0500

    more institution and transit number validation

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import { institutions } from "./institutions.js";
+const invalidBankNumber = /\D/;
+const formatBankNumber = (unformattedNumber, numberLength) => {
+    if (typeof (unformattedNumber) === "undefined") {
+        return false;
+    }
+    let formattedNumber = unformattedNumber.toString();
+    if (formattedNumber.length > numberLength) {
+        return false;
+    }
+    if (invalidBankNumber.test(formattedNumber)) {
+        return false;
+    }
+    formattedNumber = ("00000" + formattedNumber).slice(-1 * numberLength);
+    return formattedNumber;
+};
 export const getCanadianBankName = (institutionNumber, transitNumber) => {
     let bankName;
-    const institutionNumberString = ("000" + institutionNumber.toString()).slice(-3);
-    if (transitNumber) {
-        const transitNumberString = ("00000" + transitNumber.toString()).slice(-5);
+    const institutionNumberString = formatBankNumber(institutionNumber, 3);
+    if (!institutionNumberString) {
+        return undefined;
+    }
+    const transitNumberString = formatBankNumber(transitNumber, 5);
+    if (transitNumberString) {
         bankName = institutions[transitNumberString + "-" + institutionNumberString];
     }
     if (!bankName) {
--- index.ts
+++ index.ts
@@ -1,14 +1,51 @@
 import { institutions } from "./institutions.js";
 
 
+const invalidBankNumber = /\D/;
+
+const formatBankNumber = (unformattedNumber: string | number, numberLength: number): string | false => {
+
+  // Ensure a number was passed
+  if (typeof(unformattedNumber) === "undefined") {
+    return false;
+  }
+
+  let formattedNumber = unformattedNumber.toString();
+
+  // Ensure the number isn't too long
+  if (formattedNumber.length > numberLength) {
+    return false;
+  }
+
+  // Ensure the number only contains digits
+  if (invalidBankNumber.test(formattedNumber)) {
+    return false;
+  }
+
+  formattedNumber = ("00000" + formattedNumber).slice(-1 * numberLength);
+
+  return formattedNumber;
+};
+
+
+/**
+ * @param {(string|number)} institutionNumber - A three-digit Canadian bank institution number.
+ * @param {(string|number)} [transitNumber] - A five-digit Canadian bank transit (branch) number.
+ * @returns {string} - A Canadian bank name.
+ */
 export const getCanadianBankName = (institutionNumber: string | number, transitNumber?: string | number): string => {
 
   let bankName: string;
 
-  const institutionNumberString = ("000" + institutionNumber.toString()).slice(-3);
+  const institutionNumberString = formatBankNumber(institutionNumber, 3);
 
-  if (transitNumber) {
-    const transitNumberString = ("00000" + transitNumber.toString()).slice(-5);
+  if (!institutionNumberString) {
+    return undefined;
+  }
+
+  const transitNumberString = formatBankNumber(transitNumber, 5);
+
+  if (transitNumberString) {
     bankName = institutions[transitNumberString + "-" + institutionNumberString] as string;
   }
 
